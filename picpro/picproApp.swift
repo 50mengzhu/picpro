98e2a5ab8782711a0c5883041ee893b67c52f4d4
mica <daixiao2012@qq.com>
2024-07-15 17:57:30 +0800

    Initial Commit

---
+++ picpro/picproApp.swift
@@ -0,0 +1,20 @@
+//
+//  picproApp.swift
+//  picpro
+//
+//  Created by mica dai on 2024/7/15.
+//
+
+import SwiftUI
+
+@main
+struct picproApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
